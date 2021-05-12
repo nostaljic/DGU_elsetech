e2f38d4dcfb3faaca86ded54903e0a3d982e4092
DESKTOP-OJQQ6HL\Scorpion <scorpion@dgu.ac.kr>
2021-05-12 18:30:48 +0900

    main loading page is done

--- dgu_elsetech/lib/style/color.dart
+++ dgu_elsetech/lib/style/color.dart
@@ -1,3 +1,16 @@
 import 'package:flutter/material.dart';
-
 const themeBlue = Color.fromRGBO(96, 160, 209, 1);
+
+class Background extends StatelessWidget {
+  Widget build(BuildContext context) {
+    return Container(
+        decoration: BoxDecoration(
+            gradient: LinearGradient(
+              colors: [Color(0xff60A0D1), Color(0xffD9EFFF)],
+              begin: const FractionalOffset(0.0, 0.0),
+              end: const FractionalOffset(0.0, 1.0),
+            )
+        )
+    );
+  }
+}
