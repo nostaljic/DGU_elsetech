ccc39fc1390d9a2af3ce396dd5392a6b60d2b8a2
Kim-kwan-woo <kw2577@naver.com>
2021-05-18 03:44:59 +0900

    complete drawer

---
+++ dgu_elsetech/lib/style/typography.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+const TextStyle drawerBold =
+    TextStyle(fontSize: 15, color: Colors.black, fontWeight: FontWeight.bold);
+
+const TextStyle drawerBlue =
+    TextStyle(fontSize: 13, color: Color.fromRGBO(0, 60, 255, 1));
