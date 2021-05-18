bb76d7c5e0fa4adb67826cf1a511bd8e1b230f76
Scorpion <scorpion@dgu.ac.kr>
2021-05-19 02:55:59 +0900

    api 연동 및 home 화면 구성

--- dgu_elsetech/lib/main.dart
+++ dgu_elsetech/lib/main.dart
@@ -15,7 +15,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
 
-      home: AnalysisReport(),
+      home: Home(),
 
     );
   }
--- dgu_elsetech/lib/screen/home.dart
+++ dgu_elsetech/lib/screen/home.dart
@@ -2,45 +2,82 @@ import 'package:dgu_elsetech/widget/header.dart';
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'dart:async';
+import 'package:geolocator/geolocator.dart';
 
+import 'package:dgu_elsetech/widget/box_decoration.dart';
 
 class Home extends StatefulWidget {
   @override
   _HomeState createState() => _HomeState();
 }
+
 class _HomeState extends State<Home> {
+
+  Future<Position> getLocation() async {
+    Position position =
+    // ignore: deprecated_member_use
+    await getCurrentPosition(desiredAccuracy: LocationAccuracy.low);
+    return position;
+  }
+
   Completer<GoogleMapController> _controller = Completer();
 
-  static final gwanghwamun = CameraPosition(
-    target: LatLng(37.575929, 126.976849),
-    zoom: 3.0,
-  );
+
   @override
   Widget build(BuildContext context) {
     final Size size = MediaQuery.of(context).size;
+    Future<Position> nowpos = getLocation();
+    var lat = 0.0;
+    var long = 0.0;
+    var nowLoc = CameraPosition(
+      target: LatLng(100,100),
+      zoom: 20,
+    );
+
+
 
     return CustomHeader(
         body: Center(
-        child: Container(
-          alignment: AlignmentDirectional.topCenter,
-          width: size.width*0.9,
-          height: size.height*0.5,
-          color: Colors.amber,
+            child: ClipRRect(
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(25),
+                  topRight: Radius.circular(25),
+                  bottomRight: Radius.circular(30),
+                  bottomLeft: Radius.circular(30),
+                ),
+                child: Align(
+                    alignment: Alignment.topRight,
+                    heightFactor: 1.0,
+                    widthFactor: 1.0,
+                    child: nowLoc != null ? SizedBox(
 
-          child: GoogleMap(
-          mapType: MapType.hybrid,
-          initialCameraPosition: gwanghwamun,
-          onMapCreated: (GoogleMapController controller) {
-      _controller.complete(controller);
-    },
-            compassEnabled: true,
-            zoomGesturesEnabled: true,
-            rotateGesturesEnabled: true,
-            scrollGesturesEnabled: true,
-            tiltGesturesEnabled: true,
-          ),
-        ),
-        ),
+                      width:size.width*0.98,
+                      height:size.height*0.98,
+                      child: GoogleMap (
+                        mapType: MapType.normal,
+                        initialCameraPosition: nowLoc,
+                        onMapCreated: (GoogleMapController controller) {
+                          _controller.complete(controller);
+                        },
+                        compassEnabled: true,
+                        zoomGesturesEnabled: true,
+                        rotateGesturesEnabled: true,
+                        scrollGesturesEnabled: true,
+                        tiltGesturesEnabled: true,
+                        myLocationEnabled: true,
+                        myLocationButtonEnabled: true,
+
+                      ),
+                    ):null,
+                )
+            )
+        )
     );
   }
 }
+
+/*
+*
+          *
+          *
+          * */
