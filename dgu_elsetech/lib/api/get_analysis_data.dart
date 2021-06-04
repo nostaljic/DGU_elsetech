0f10ffff0327c2ad5b54b3a10def1ad31427169a
Kim-kwan-woo <kw2577@naver.com>
2021-06-05 02:03:24 +0900

    decode response body

--- dgu_elsetech/lib/api/get_analysis_data.dart
+++ dgu_elsetech/lib/api/get_analysis_data.dart
@@ -1,19 +1,26 @@
 import 'dart:io';
-
+import 'dart:convert';
 import 'package:http/http.dart' as http;
+
+// Future<String> getCsrftoken() async {
+//   var response =
+//   await http.get(Uri.encodeFull('http://10.0.2.2:8000/admin/'));
+//   var csrftoken =
+//   response.headers.remove('set-cookie').substring(10, 74); //csrf 64 chars
+//   return csrftoken;
+// }
 
 postRequest() async {
   String url = 'http://10.0.2.2:8000/admin/get_water_info/analysisdata/';
 
-  await http.post(url, headers: {
+  var response = await http.post(Uri.encodeFull(url), headers: <String, String>{
     'Content-Type': 'application/x-www-form-urlencoded',
-    HttpHeaders.authorizationHeader: 'csrf_token'
-  }, body: {
+  }, body: <String, String>{
     'sql': 'select * from AnalysisData'
   }).then((response) {
-    print(response.body);
+    print(utf8.decode(response.bodyBytes));
   });
-  //return response;
+  return response;
 }
 
 Future<http.Response> postRequest_user(user) async {
